b888df86c55e5250c1b87ca679ff0bec24940942
jodijhouranda <jwst.28@gmail.com>
2026-02-17 15:21:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -147,6 +147,7 @@ def get_location_description(lat, lng):
 # Geolocation & UI state
 if 'user_lat' not in st.session_state: st.session_state.user_lat = None
 if 'user_lng' not in st.session_state: st.session_state.user_lng = None
+if 'use_location_toggle' not in st.session_state: st.session_state.use_location_toggle = False
 if 'resolved_address' not in st.session_state: st.session_state.resolved_address = None
 if 'loc_step' not in st.session_state: st.session_state.loc_step = 0 # 0: None, 1: GPS, 2: Geocoding
 
